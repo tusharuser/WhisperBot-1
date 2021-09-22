86b18092189fd40dcf36d7f5b2cedf9fbd0de8f6
tusharuser <71004019+tusharuser@users.noreply.github.com>
2021-09-22 21:02:02 +0530

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -15,7 +15,7 @@ app = Client(
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="LovelyBot"),
+    plugins=dict(root="WhisperBot"),
 )
 
 
